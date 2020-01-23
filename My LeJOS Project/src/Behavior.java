39d02b36b82aeabe7547904b1c6eb5588e9ca61a
MrInterBugs <aedan@mrinterbugs.uk>
2020-01-23 20:43:02 +0000

    Four and Five, Finished and Started.

---
+++ My LeJOS Project/src/Behavior.java
@@ -0,0 +1,9 @@
+
+public class Behavior {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ My LeJOS Project/src/PilotSquare.java
@@ -0,0 +1,65 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.localization.PoseProvider;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.navigation.Waypoint;
+import lejos.robotics.pathfinding.Path;
+
+public class PilotSquare {
+	
+	final static float WHEEL_DIAMETER = 56; // The diameter (mm) of the wheels
+	final static float AXLE_LENGTH = 120; // The distance (mm) your two driven wheels
+	final static float ANGULAR_SPEED = 100; // How fast around corners (degrees/sec)
+	final static float LINEAR_SPEED = 70; // How fast in a straight line (mm/sec)
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*		final int SQUARE_ANGLE = 90;
+		final int FORWARDS_DISTANCE = 1000;
+		final int SIDES = 4;
+*/		
+		
+		BaseRegulatedMotor mL = new EV3LargeRegulatedMotor(MotorPort.A);
+		Wheel wL = WheeledChassis.modelWheel(mL, WHEEL_DIAMETER).offset(AXLE_LENGTH/2);
+		
+		BaseRegulatedMotor mR = new EV3LargeRegulatedMotor(MotorPort.B);
+		Wheel wR = WheeledChassis.modelWheel(mR, WHEEL_DIAMETER).offset(-AXLE_LENGTH/2);
+		
+		Chassis chassis = new WheeledChassis(new Wheel[]{wR,wL},WheeledChassis.TYPE_DIFFERENTIAL);
+		MovePilot plt = new MovePilot(chassis);
+		PoseProvider poseP = new OdometryPoseProvider(plt);
+		
+		plt.setLinearSpeed(LINEAR_SPEED);
+		plt.setAngularSpeed(ANGULAR_SPEED);
+		
+/*		for (int sides = 0; sides < SIDES; sides++) {
+			plt.travel(FORWARDS_DISTANCE);
+			plt.rotate(SQUARE_ANGLE);
+		}
+*/
+		Navigator navigator = new Navigator(plt, poseP);
+		
+		Path path = new Path();
+		path.add(new Waypoint(100, 0));
+		path.add(new Waypoint(100, 100));
+		path.add(new Waypoint(0, 100));
+		path.add(new Waypoint(0, 0));
+		
+		navigator.followPath(path);
+		navigator.waitForStop();
+		
+		plt.stop();
+		LCD.drawString(poseP.getPose().toString(),0,0);
+		Button.ENTER.waitForPressAndRelease();
+	}
+
+}
