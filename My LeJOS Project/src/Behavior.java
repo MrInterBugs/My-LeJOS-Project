d2ca12f454050f8f637fc2d20c444f2f0751fe79
MrInterBugs <aedan@mrinterbugs.uk>
2020-01-24 10:39:58 +0000

    Added javadoc.

--- My LeJOS Project/src/Behavior.java
+++ My LeJOS Project/src/Behavior.java
@@ -12,11 +12,30 @@ import lejos.robotics.localization.PoseProvider;
 import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
 
+/**
+ * Program to demonstrate different behaviours for our Lego robot.
+ * 
+ * @author Aedan Lawrence (2019)
+ */
 public class Behavior {
 	
-	static final double WHEEL_DIAMETER = 56; // The diameter (mm) of the wheels
-	static final double AXLE_LENGTH = 120; // The distance (mm) your two driven wheels
-
+	/**
+	 * Displays the program and version information until a button is pressed.
+	 */
+	public static void init() {
+		LCD.drawString("Behavior.jar",2,2);
+		LCD.drawString("Version 0.1",2,3);
+		LCD.drawString("Aedan Lawrence",2,4);
+		LCD.drawString("Press Enter",2,5);
+		Button.ENTER.waitForPressAndRelease();
+		LCD.clear();
+	}
+	
+	/**
+	 * @param args
+	 * 
+	 * Main method to hold constants and call other methods.
+	 */
 	public static void main(String[] args) {
 		final int ONE_METER = 1000;
 		
@@ -30,74 +49,75 @@ public class Behavior {
 		final int ANGULAR_SPEED_DAY = 140; // How fast around corners in bright light(degrees/sec)
 		final int LINEAR_SPEED_DAY = 200; // How fast in a straight line in bright light(mm/sec)
 		
-		LCD.drawString("Behavior.java",2,2);
-		LCD.drawString("Version 0.1",2,3);
-		LCD.drawString("Aedan Lawrence",2,4);
-		Button.ENTER.waitForPressAndRelease();
-		LCD.clear();
-		
-		move(LINEAR_SPEED_DAY, ONE_METER);
-		turn(U_TURN, ANGULAR_SPEED_DAY);
-		Delay.msDelay(SHORT_PAUSE);
-		move(LINEAR_SPEED_NIGHT, -ONE_METER);
-		turn(U_TURN, ANGULAR_SPEED_NIGHT);
-	}
-	
-	/*
-	* Allows the creation of a movePilot to control the movements of the robot.
-	* 
-	* @return pilot
-	*/
-	public static MovePilot pilot() {
+		final double WHEEL_DIAMETER = 56; // The diameter (mm) of the wheels
+		final double AXLE_LENGTH = 120; // The distance (mm) your two driven wheels
+
 		BaseRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 		Wheel leftWheel = WheeledChassis.modelWheel(leftMotor, WHEEL_DIAMETER).offset(AXLE_LENGTH/2);
 		
 		BaseRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 		Wheel rightWheel = WheeledChassis.modelWheel(rightMotor, WHEEL_DIAMETER).offset(-AXLE_LENGTH/2);
 		
+		final MovePilot PILOT = pilot(rightWheel, leftWheel);
+		
+		init();
+		
+		move(LINEAR_SPEED_DAY, ONE_METER, PILOT);
+		turn(U_TURN, ANGULAR_SPEED_DAY, PILOT);
+		Delay.msDelay(SHORT_PAUSE);
+		move(LINEAR_SPEED_NIGHT, ONE_METER, PILOT);
+		turn(U_TURN, ANGULAR_SPEED_NIGHT, PILOT);
+	}
+	
+	/**
+	 * Allows the creation of a movePilot to control the movements of the robot.
+	 * 
+	 * @return the move pilot.
+	 */
+	public static MovePilot pilot(Wheel leftWheel, Wheel rightWheel) {
 		Chassis chassis = new WheeledChassis(new Wheel[]{rightWheel,leftWheel},WheeledChassis.TYPE_DIFFERENTIAL);
 		return(new MovePilot(chassis));
 	}
 	
-	/*
-	* @input Speed
-	* @input DistanceToTravel
-	* 
-	* Using a movePilot moves the robot at a set speed in a straight line.
-	*/
-	public static void move(int SPEED, int TRAVEL_DISTANCE) {
+	/**
+	 * @param SPEED The speed which the robot moves at.
+	 * @param TRAVEL_DISTANCE Sets the total distance to travel in mm.
+	 * @param PILOT The movePilot to control the motors.
+	 * 
+	 * Using a movePilot moves the robot at a set speed in a straight line.
+	 */
+	public static void move(int SPEED, int TRAVEL_DISTANCE, MovePilot PILOT) {
 		LCD.clear();
 		
-		MovePilot pilot = pilot();
-		PoseProvider poseProvider = new OdometryPoseProvider(pilot);
+		PoseProvider poseProvider = new OdometryPoseProvider(PILOT);
 		
-		pilot.setLinearSpeed(SPEED);
+		PILOT.setLinearSpeed(SPEED);
 
 		Sound.beep();
-		pilot.travel(TRAVEL_DISTANCE);
+		PILOT.travel(TRAVEL_DISTANCE);
 		
-		pilot.stop();
+		PILOT.stop();
 		LCD.drawString(poseProvider.getPose().toString(),0,0);
 	}
 	
-	/*
-	* @input AngleToTurn
-	* @input Speed
-	* 
-	* Using a movePilot rotates the robot through an angle at a set speed.
-	*/
-	public static void turn(int ANGLE, int SPEED) {
+	/**
+	 * @param ANGLE The angle to turn the robot by.
+	 * @param SPEED The speed at which to do the turn at.
+	 * @param PILOT The movePilot to control the motors.
+	 * 
+	 * Using a movePilot rotates the robot through an angle at a set speed.
+	 */
+	public static void turn(int ANGLE, int SPEED, MovePilot PILOT) {
 		LCD.clear();
 		
-		MovePilot pilot = pilot();
-		PoseProvider poseProvider = new OdometryPoseProvider(pilot);
+		PoseProvider poseProvider = new OdometryPoseProvider(PILOT);
 		
-		pilot.setAngularSpeed(SPEED);
+		PILOT.setAngularSpeed(SPEED);
 
 		Sound.beep();
-		pilot.rotate(ANGLE);
+		PILOT.rotate(ANGLE);
 		
-		pilot.stop();
+		PILOT.stop();
 		LCD.drawString(poseProvider.getPose().toString(),0,0);
 	}
 }
